e1e64f960a9fdd520c34075e7b7d4a8091446376
Aris <titilayotijani15@gmail.com>
2024-09-26 16:46:02 +0100

    fix to design file width

--- src/script.js
+++ src/script.js
@@ -2,12 +2,10 @@ function hideContainer() {
   document
     .querySelectorAll(".hide")
     .forEach((element) => (element.style.display = "none"));
-  /*document.querySelector(".updates").style.display = "none";*/
 }
 
 function showContainer() {
   document
     .querySelectorAll(".hide")
     .forEach((element) => (element.style.display = ""));
-  /*document.querySelector(".updates").style.display = "";*/
 }
